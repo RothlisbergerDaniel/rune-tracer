09ca61723eb4c2156bfbe69d6f270763db73e2e4
RothlisbergerDaniel <dlrothlis@gmail.com>
2024-03-06 15:38:06 -0500

    Finished rune lineup, squished some bugs, added a bunch of comments to the code

--- microMixTemplate/Assets/Team01/Scripts/Trace.cs
+++ microMixTemplate/Assets/Team01/Scripts/Trace.cs
@@ -25,19 +25,22 @@ namespace team01
         public float cursorSpeed = 100f; //speed at which the cursor snaps to points, default 10
 
         public int[] runeDifficulties = new int[10]; //list of rune difficulties by ID in order from low to high
-        public int maxCount;
+        public int[] runeTypes; //list of rune types
+        public int maxCount; //max number of runes to complete
+        int currentRune; //current rune in sequence
         
         // Start is called before the first frame update
         void Start()
         {
             //getStepPosition(runeID);
-            getNewRune(false, true); //replace this with hiding cursor on Start, then show cursor OnGameStart
+            runeTypes = new int[10];
             getLineup();
+            getNewRune(runeTypes[currentRune], true); //replace this with hiding cursor on Start, then show cursor OnGameStart
         }
         protected override void OnGameStart()
         {
             //base.OnGameStart();
-            getNewRune(false, false);
+            getNewRune(runeTypes[currentRune], false);
         }
 
 
@@ -57,7 +60,15 @@ namespace team01
                 } 
                 else
                 {
-                    getNewRune(true, false); //gets a new random rune - this will be changed to get the next rune in a random sequence
+                    currentRune++;
+                    if (currentRune < maxCount)
+                    {
+                        getNewRune(runeTypes[currentRune], false); //gets the next rune in the sequence
+                    }
+                    else
+                    {
+                        ReportGameCompletedEarly(); //end game if all runes are completed early
+                    }
                 }
                 
 
@@ -68,7 +79,15 @@ namespace team01
             }
             else if (stepCount == runeStepCounts[runeID] && direction == Vector2.zero)
             {
-                getNewRune(true, false); //gets a new random rune - this will be changed to get the next rune in a random sequence
+                currentRune++;
+                if (currentRune < maxCount)
+                {
+                    getNewRune(runeTypes[currentRune], false);
+                }
+                else
+                {
+                    ReportGameCompletedEarly();
+                }
             }
         }
 
@@ -83,18 +102,19 @@ namespace team01
             //return stepPos;
         }
 
-        void getNewRune(bool random, bool onFirstLoad)
+        void getNewRune(int type, bool onFirstLoad)
         {
-            if(random)
-            {
-                runeID = Random.Range(0, 10); //only update runeID if specified. This allows for easy testing of specific runes.
-            }
+            //if(random)
+            //{
+            //    runeID = Random.Range(0, 10); //only update runeID if specified. This allows for easy testing of specific runes.
+            //}
+            runeID = type;
             stepCount = 0; //reset step count
             getStepPosition(runeID); //get actual step position
             transform.position = new Vector3(runeStarts[runeID][0] - 0f, runeStarts[runeID][1] - 1.5f, 0f); //reset cursor transform, adjusting for new position
             target = runeStepInputs[stepPos]; //set first target direction for new rune
-            linkedRune.GetComponent<Rune>().type = runeID;
-            if(!onFirstLoad)
+            linkedRune.GetComponent<Rune>().type = runeID; //sets linked rune's type
+            if(!onFirstLoad) //prevent errors by not updating on first load
             {
                 linkedRune.SendMessage("updateSprite", runeID);
             }
@@ -102,14 +122,31 @@ namespace team01
 
         void getLineup()
         {
-           for(int i = 0; i < maxCount; i++)
+            for(int i = 0; i < maxCount; i++)
+            {
+                if(i < Mathf.Round(maxCount / 10 * 4))
+                {
+                    runeTypes[i] = runeDifficulties[Random.Range(0, 4)]; //grab the rune type from a list of difficulties with a difficulty value of 1
+                }
+                else if(i < Mathf.Round(maxCount / 10 * 8))
+                {
+                    runeTypes[i] = runeDifficulties[Random.Range(4, 8)]; //difficulty 2
+                }
+                else
+                {
+                    runeTypes[i] = runeDifficulties[Random.Range(8, 10)]; //difficulty 3
+                }
+            }
+               
+            for(int i = 0; i < maxCount; i++)
             {
                 GameObject currentRuneItem;
                 currentRuneItem = Instantiate(lineupRune, new Vector3(-2.25f + (i * 0.5f), -4.5f, 0f), Quaternion.identity); //instantiate runes at positions 0.5x apart from each other
                 currentRuneItem.GetComponent<Rune>().id = i;   //get the script and set id
-                currentRuneItem.GetComponent<Rune>().type = i; //as well as rune type
-                //currentRuneItem.SendMessage("updateSprite", i, SendMessageOptions.DontRequireReceiver);
+                currentRuneItem.GetComponent<Rune>().type = runeTypes[i]; //as well as rune type
+                //currentRuneItem.SendMessage("updateSprite", i, SendMessageOptions.DontRequireReceiver); //uncomment this to throw an error :skull:
             }
+            currentRune = 0; //reset current rune
         }
     }
 }    
