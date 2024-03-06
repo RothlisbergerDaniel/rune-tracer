250809b0fafc5fc664a20425d0bad8d3eca17ada
RothlisbergerDaniel <dlrothlis@gmail.com>
2024-03-06 13:29:43 -0500

    Added rune lineup at the bottom, and fixed a particularly nasty bug relating to script execution order.

--- microMixTemplate/Assets/Team01/Scripts/Rune.cs
+++ microMixTemplate/Assets/Team01/Scripts/Rune.cs
@@ -7,12 +7,15 @@ namespace team01
     public class Rune : MicrogameInputEvents
     {
 
-        public Sprite[] runeSprites;
-        SpriteRenderer sr;
+        public Sprite[] runeSprites; //stores all graphics
+        SpriteRenderer sr; //gets SpriteRenderer to set graphic
+        public int id;   //rune ID
+        public int type; //and type, to be accessed and set when instantiating lineup prefabs
         // Start is called before the first frame update
         void Start()
         {
             sr = GetComponent<SpriteRenderer>();
+            updateSprite(type);
         }
 
         // Update is called once per frame
--- microMixTemplate/Assets/Team01/Scripts/Trace.cs
+++ microMixTemplate/Assets/Team01/Scripts/Trace.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem.Controls;
 
@@ -16,21 +17,27 @@ namespace team01
         int stepCount = 0; //tracks current steps taken to compare against current rune's total step count
 
         public int runeID = 0; //public for testing purposes
-        public GameObject linkedRune;
+        public GameObject linkedRune; //rune linked to this copy of the game controller
+        public GameObject lineupRune; //links to the lineup rune prefab
 
-        Vector2 direction;
-        Vector2 target;
-        public float cursorSpeed = 1f;
+        Vector2 direction; //current user direction input
+        Vector2 target; //target direction input
+        public float cursorSpeed = 100f; //speed at which the cursor snaps to points, default 10
+
+        public int[] runeDifficulties = new int[10]; //list of rune difficulties by ID in order from low to high
+        public int maxCount;
+        
         // Start is called before the first frame update
         void Start()
         {
             //getStepPosition(runeID);
-            getNewRune(false); //replace this with hiding cursor on Start, then show cursor OnGameStart
+            getNewRune(false, true); //replace this with hiding cursor on Start, then show cursor OnGameStart
+            getLineup();
         }
         protected override void OnGameStart()
         {
             //base.OnGameStart();
-            getNewRune(false);
+            getNewRune(false, false);
         }
 
 
@@ -38,7 +45,7 @@ namespace team01
         void Update()
         {
             direction = stick; //direction variable is set to stick input
-            Vector3 currentPoint = new Vector3 (runePoints[Mathf.Clamp(stepPos - 1, 0, 100)][0] - 0f, runePoints[Mathf.Clamp(stepPos - 1, 0, 100)][1] - 1.5f, 0f); //convert preset point to new position, scalabale to 2P
+            Vector3 currentPoint = new Vector3(runePoints[Mathf.Clamp(stepPos - 1, 0, 100)][0] - 0f, runePoints[Mathf.Clamp(stepPos - 1, 0, 100)][1] - 1.5f, 0f); //convert preset point to new position, scalabale to 2P
             if (direction == target && (transform.position == currentPoint || stepCount == 0)) //use Mathf.Clamp to ensure no index out-of-bounds errors
             {
                 if (stepCount < runeStepCounts[runeID])
@@ -46,11 +53,11 @@ namespace team01
                     stepPos++;
                     //stepPos = Mathf.Clamp(stepPos, 0, runePoints.Length); //clamp so it doesn't go out-of-bounds
                     stepCount++; //update actual and virtual step position
-                    target = runeStepInputs[stepPos];
+                    target = runeStepInputs[stepPos]; //get new target input
                 } 
                 else
                 {
-                    getNewRune(true); //gets a new random rune - this will be changed to get the next rune in a random sequence
+                    getNewRune(true, false); //gets a new random rune - this will be changed to get the next rune in a random sequence
                 }
                 
 
@@ -61,7 +68,7 @@ namespace team01
             }
             else if (stepCount == runeStepCounts[runeID] && direction == Vector2.zero)
             {
-                getNewRune(true); //gets a new random rune - this will be changed to get the next rune in a random sequence
+                getNewRune(true, false); //gets a new random rune - this will be changed to get the next rune in a random sequence
             }
         }
 
@@ -76,7 +83,7 @@ namespace team01
             //return stepPos;
         }
 
-        void getNewRune(bool random)
+        void getNewRune(bool random, bool onFirstLoad)
         {
             if(random)
             {
@@ -84,9 +91,25 @@ namespace team01
             }
             stepCount = 0; //reset step count
             getStepPosition(runeID); //get actual step position
-            transform.position = new Vector3 (runeStarts[runeID][0] - 0f, runeStarts[runeID][1] - 1.5f, 0f); //reset cursor transform, adjusting for new position
+            transform.position = new Vector3(runeStarts[runeID][0] - 0f, runeStarts[runeID][1] - 1.5f, 0f); //reset cursor transform, adjusting for new position
             target = runeStepInputs[stepPos]; //set first target direction for new rune
-            linkedRune.SendMessage("updateSprite", runeID);
+            linkedRune.GetComponent<Rune>().type = runeID;
+            if(!onFirstLoad)
+            {
+                linkedRune.SendMessage("updateSprite", runeID);
+            }
+        }
+
+        void getLineup()
+        {
+           for(int i = 0; i < maxCount; i++)
+            {
+                GameObject currentRuneItem;
+                currentRuneItem = Instantiate(lineupRune, new Vector3(-2.25f + (i * 0.5f), -4.5f, 0f), Quaternion.identity); //instantiate runes at positions 0.5x apart from each other
+                currentRuneItem.GetComponent<Rune>().id = i;   //get the script and set id
+                currentRuneItem.GetComponent<Rune>().type = i; //as well as rune type
+                //currentRuneItem.SendMessage("updateSprite", i, SendMessageOptions.DontRequireReceiver);
+            }
         }
     }
 }    
