60d1d9a1d73885d95eb9cbf84fe3344e6282ae07
RothlisbergerDaniel <dlrothlis@gmail.com>
2024-03-07 15:01:32 -0500

    Made sure TestScript.cs doesn't cause stupid problems going forwards.

--- microMixTemplate/Assets/Team01/Scripts/TestScript.cs
+++ microMixTemplate/Assets/Team01/Scripts/TestScript.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestScript : MicrogameInputEvents
+namespace team01
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
